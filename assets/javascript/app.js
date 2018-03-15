97d5ae99aa06baf4f955d8fb72d62c6ef9c814b8
Samuel Falcon <saxclar1025@gmail.com>
2018-03-15 08:57:14 -0400

    Linked necessary libraries and project files

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCAsHh9AA8FxJzCfXibfsbxur4BWHQJjCs",
+    authDomain: "roshambo-3293b.firebaseapp.com",
+    databaseURL: "https://roshambo-3293b.firebaseio.com",
+    projectId: "roshambo-3293b",
+    storageBucket: "roshambo-3293b.appspot.com",
+    messagingSenderId: "715476833630"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
